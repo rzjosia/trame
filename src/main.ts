8116b148057db73d0f558a75340a37271197266c
RAZAFINJATOVO Josia <rjosia@gmail.com>
2024-05-23 12:20:45 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,63 @@
+import "./style.scss";
+import { format } from "./trame";
+
+const lyricsInputElement = document.querySelector(
+  "#lyricsInput",
+) as HTMLTextAreaElement;
+const lyricsOutputElement = document.querySelector(
+  "#lyricsOutput",
+) as HTMLTextAreaElement;
+const countLineElement = document.querySelector(
+  "#countLine",
+) as HTMLInputElement;
+const lineLengthElement = document.querySelector(
+  "#lineLength",
+) as HTMLInputElement;
+const copyElement = document.querySelector("#copyLyrics") as HTMLButtonElement;
+const resetElement = document.querySelector(
+  "#resetLyrics",
+) as HTMLButtonElement;
+const titleElement = document.querySelector("#songTitle") as HTMLInputElement;
+
+const updateLyricsOutput = function (e: Event) {
+  e.preventDefault();
+  const title = titleElement.value;
+  const lyrics = lyricsInputElement.value;
+  const countLine = countLineElement.valueAsNumber;
+  const lineLength = lineLengthElement.valueAsNumber;
+  lyricsOutputElement.value = format(title, lyrics, countLine, lineLength);
+};
+
+const elements = [
+  titleElement,
+  lyricsInputElement,
+  countLineElement,
+  lineLengthElement,
+];
+
+elements.forEach((element) => {
+  element.addEventListener("keyup", updateLyricsOutput, false);
+  element.addEventListener("change", updateLyricsOutput, false);
+});
+
+copyElement.addEventListener(
+  "click",
+  async function (e) {
+    e.preventDefault();
+    lyricsOutputElement.select();
+    lyricsOutputElement.setSelectionRange(0, 99999);
+    await navigator.clipboard.writeText(lyricsOutputElement.value);
+  },
+  false,
+);
+
+resetElement.addEventListener(
+  "click",
+  function (e) {
+    e.preventDefault();
+    titleElement.value = "";
+    lyricsInputElement.value = "";
+    lyricsOutputElement.value = "";
+  },
+  false,
+);
---
+++ src/trame.ts
@@ -0,0 +1,50 @@
+export function format(
+  title: string,
+  lyrics: string,
+  splitLine = 2,
+  lineLength = 35,
+) {
+  if ("" === title.trim() && "" === lyrics.trim()) {
+    return "";
+  }
+
+  const verseRegex = /Strophe/gim;
+  const numericVerseRegex = /^(\d+)[.-\s]*(.+)/gim;
+  const sections = ["Couplet", "Refrain", "Pont", "Pré-refrain"].join("|");
+  const sectionRegex = new RegExp(`^(${sections})(\\s*\\d*)\\n`, "gim");
+  const sectionMatcher = `\\[(${sections})\\s*\\d*\\]\\n+`;
+  const matcher = new RegExp(
+    `(${sectionMatcher})?((.+\\n+)(?<!(${sectionMatcher}))){1,${splitLine}}`,
+    "gim",
+  );
+
+  const formated =
+    wrapText(lyrics, lineLength)
+      .join("\n")
+      .replace(numericVerseRegex, "Couplet $1\n$2\n")
+      .replace(verseRegex, "Couplet")
+      .replace(sectionRegex, "[$1$2]\n")
+      .concat("\n")
+      .replace(/\n+/gi, "\n")
+      .match(matcher)
+      ?.join("\n")
+      .trim() ?? "";
+
+  if ("" === title) {
+    const firstLineRegex = new RegExp(`^(?!${sectionMatcher}).+`, "mi");
+    const firstLineMatch = formated.match(firstLineRegex) || [""];
+    title = firstLineMatch[0];
+  }
+
+  return `[Introduction]\n${title}\n\n${formated}\n\n[Conclusion]\n\n`;
+}
+
+function wrapText(text: string, chunkSize = 28): string[] {
+  const regex = new RegExp(`.{1,${chunkSize}}(\\s|$)`, "g");
+  return (
+    text
+      .trim()
+      .match(regex)
+      ?.map((subText) => subText.trim()) || []
+  );
+}
